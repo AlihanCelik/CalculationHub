a0b356820ac17df0528f9ccb567b62962cfd6bbf
Alihan <alihan_08032003@hotmail.com>
2023-06-19 22:05:02 +0300

    Calculator

---
+++ app/src/main/java/com/example/calculatorapp/CookingCalculatorActivity.kt
@@ -0,0 +1,11 @@
+package com.example.calculatorapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CookingCalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cooking_calculator)
+    }
+}
