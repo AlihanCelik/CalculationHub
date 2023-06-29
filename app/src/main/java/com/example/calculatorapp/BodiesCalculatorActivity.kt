ba926e7d1c42d370cfcc048bc9a0ac8895ce98c6
Alihan <alihan_08032003@hotmail.com>
2023-06-29 15:23:57 +0300

    Calculator

---
+++ app/src/main/java/com/example/calculatorapp/BodiesCalculatorActivity.kt
@@ -0,0 +1,12 @@
+package com.example.calculatorapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BodiesCalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bodies_calculator)
+
+    }
+}
