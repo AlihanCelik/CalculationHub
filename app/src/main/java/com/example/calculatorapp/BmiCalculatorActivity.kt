684905435d6a4387226ecae8b1c3d9abcf2610f0
Alihan <alihan_08032003@hotmail.com>
2023-06-30 20:37:26 +0300

    Calculator

--- app/src/main/java/com/example/calculatorapp/BmiCalculatorActivity.kt
+++ app/src/main/java/com/example/calculatorapp/BmiCalculatorActivity.kt
@@ -1,29 +1,33 @@
 package com.example.calculatorapp
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.SeekBar
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import kotlinx.android.synthetic.main.activity_bmi_calculator.*
+import kotlinx.android.synthetic.main.activity_bmi_calculator.male
+import kotlinx.android.synthetic.main.dialog_bmi_solutions.view.*
 
 
 class BmiCalculatorActivity : AppCompatActivity() {
-    var girl=false
-    var man=false
-    var heightvalue=0.0
-    var weightvalue=0.0
-    var agevalue=0
-    var bmivalue=0.0
-    var statusBmi="gender"
+    var girl = false
+    var man = false
+    var heightvalue = 0.0
+    var weightvalue = 0.0
+    var agevalue = 0
+    var bmivalue = 0.0
+    var statusBmi = "gender"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bmi_calculator)
-
+        backButton.setOnClickListener {
+            finish()
+        }
         weightSelect.setOnClickListener {
 
             val bottomSheet =
@@ -52,17 +56,21 @@ class BmiCalculatorActivity : AppCompatActivity() {
             bottomSheet.show()
         }
 
-        heightSelect.setOnClickListener{
-            val bottomSheet=BottomSheetDialog(this@BmiCalculatorActivity,R.style.BottomSheetDialogTheme)
-            val bottomSheetView=LayoutInflater.from(applicationContext)
-                .inflate(R.layout.bottom_sheet_height,findViewById(R.id.bottomSheet))as ConstraintLayout
-            bottomSheetView.findViewById<View>(R.id.cm).setOnClickListener{
-                viewHeight.text="cm"
+        heightSelect.setOnClickListener {
+            val bottomSheet =
+                BottomSheetDialog(this@BmiCalculatorActivity, R.style.BottomSheetDialogTheme)
+            val bottomSheetView = LayoutInflater.from(applicationContext)
+                .inflate(
+                    R.layout.bottom_sheet_height,
+                    findViewById(R.id.bottomSheet)
+                ) as ConstraintLayout
+            bottomSheetView.findViewById<View>(R.id.cm).setOnClickListener {
+                viewHeight.text = "cm"
 
                 bottomSheet.dismiss()
             }
-            bottomSheetView.findViewById<View>(R.id.metre).setOnClickListener{
-                viewHeight.text="m"
+            bottomSheetView.findViewById<View>(R.id.metre).setOnClickListener {
+                viewHeight.text = "m"
                 val minValue = 0.5
                 val maxValue = 2.5
                 val stepSize = 0.1
@@ -77,12 +85,12 @@ class BmiCalculatorActivity : AppCompatActivity() {
 
                 bottomSheet.dismiss()
             }
-            bottomSheetView.findViewById<View>(R.id.fit).setOnClickListener{
-                viewHeight.text="ft"
+            bottomSheetView.findViewById<View>(R.id.fit).setOnClickListener {
+                viewHeight.text = "ft"
                 bottomSheet.dismiss()
             }
-            bottomSheetView.findViewById<View>(R.id.inch).setOnClickListener{
-                viewHeight.text="in"
+            bottomSheetView.findViewById<View>(R.id.inch).setOnClickListener {
+                viewHeight.text = "in"
                 bottomSheet.dismiss()
             }
             bottomSheetView.findViewById<View>(R.id.linearLayout2).setOnClickListener {
@@ -92,32 +100,32 @@ class BmiCalculatorActivity : AppCompatActivity() {
             bottomSheet.show()
         }
         male.setOnClickListener {
-            val color=resources.getColor(R.color.yellow)
-            val color2=resources.getColor(R.color.grey)
+            val color = resources.getColor(R.color.yellow)
+            val color2 = resources.getColor(R.color.grey)
             manImage.setColorFilter(color)
             manText.setTextColor(color)
             girlImage.setColorFilter(color2)
             girlText.setTextColor(color2)
-            man=true
-            girl=false
+            man = true
+            girl = false
         }
 
         female.setOnClickListener {
-            val color2=resources.getColor(R.color.yellow)
-            val color=resources.getColor(R.color.grey)
+            val color2 = resources.getColor(R.color.yellow)
+            val color = resources.getColor(R.color.grey)
             manImage.setColorFilter(color)
             manText.setTextColor(color)
             girlImage.setColorFilter(color2)
             girlText.setTextColor(color2)
-            girl=true
-            man=false
+            girl = true
+            man = false
         }
-        seekbarforage.max=130
-        seekbarforage.progress=0
-        seekbarforage.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+        seekbarforage.max = 130
+        seekbarforage.progress = 0
+        seekbarforage.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                val progres:String=progress.toString()
-                textAge.text=progres
+                val progres: String = progress.toString()
+                textAge.text = progres
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
@@ -130,14 +138,13 @@ class BmiCalculatorActivity : AppCompatActivity() {
 
         })
         linearLayout2.setOnClickListener {
-            if(!man && !girl){
-                Toast.makeText(this,"Select Your Gender First", Toast.LENGTH_SHORT).show();
-            }
-            else if(heigh_value.text.toString() == "00"){
-                Toast.makeText(this,"Height is Incorrect", Toast.LENGTH_SHORT).show();
-            }else if(weight_value.text.toString() == "00"){
-                Toast.makeText(this,"Weight is Incorrect", Toast.LENGTH_SHORT).show();
-            }else {
+            if (!man && !girl) {
+                Toast.makeText(this, "Select Your Gender First", Toast.LENGTH_SHORT).show();
+            } else if (heigh_value.text.toString() == "") {
+                Toast.makeText(this, "Height is Incorrect", Toast.LENGTH_SHORT).show();
+            } else if (weight_value.text.toString() == "") {
+                Toast.makeText(this, "Weight is Incorrect", Toast.LENGTH_SHORT).show();
+            } else {
                 if (viewHeight.text.toString() == "cm") {
                     heightvalue = heigh_value.text.toString().toDouble()
                 } else if (viewHeight.text.toString() == "m") {
@@ -157,69 +164,63 @@ class BmiCalculatorActivity : AppCompatActivity() {
                 if (viewWeight.text.toString() == "kg") {
                     var text = weight_value.text.toString().toDouble()
                     weightvalue = text
-                } else{
+                } else {
                     var text = weight_value.text.toString().toDouble()
-                    text*=0.453
-                    weightvalue=text
+                    text *= 0.453
+                    weightvalue = text
 
                 }
-                bmivalue= (weightvalue/(((heightvalue)/100)*((heightvalue)/100)))
-                if(bmivalue<18.5){
-                    statusBmi="Thin"
-                }else if (bmivalue<=25.0 && bmivalue>18.5){
-                    statusBmi="Normal"
-                }else if (bmivalue>25){
-                    statusBmi="Fat"
-                }else{
-                    Toast.makeText(this,"You entered an invalid value", Toast.LENGTH_SHORT).show();
+                bmivalue = (weightvalue / (((heightvalue) / 100) * ((heightvalue) / 100)))
+                if (bmivalue < 18.5) {
+                    statusBmi = "Thin"
+                } else if (bmivalue <= 25.0 && bmivalue > 18.5) {
+                    statusBmi = "Normal"
+                } else if (bmivalue > 25) {
+                    statusBmi = "Fat"
+                } else {
+                    Toast.makeText(this, "You entered an invalid value", Toast.LENGTH_SHORT).show();
                 }
 
-                if(statusBmi=="Thin" ||statusBmi=="Normal"||statusBmi=="Fat"){
-                    agevalue=textAge.text.toString().toInt()
+                if (statusBmi == "Thin" || statusBmi == "Normal" || statusBmi == "Fat") {
+                    agevalue = textAge.text.toString().toInt()
 
-                    val bottomSheet=BottomSheetDialog(this@BmiCalculatorActivity,R.style.BottomSheetDialogTheme)
-                    val bottomSheetView=LayoutInflater.from(applicationContext)
-                        .inflate(R.layout.bottom_sheet_bmi,findViewById(R.id.bottomSheetBmi))as ConstraintLayout
-                    val resultBmiTextView =bottomSheetView.findViewById<TextView>(R.id.resulBmi)
-                    resultBmiTextView.text=bmivalue.toString()
-                    val statusBmiText=bottomSheetView.findViewById<TextView>(R.id.status)
-                    statusBmiText.text=statusBmi
-
-                    if(statusBmi=="Thin"){
-                        resultBmiTextView.setTextColor(resources.getColor(R.color.light_blue))
-                    }else if(statusBmi=="Normal"){
-                        resultBmiTextView.setTextColor(resources.getColor(R.color.green))
-
-                    }else{
-                        resultBmiTextView.setTextColor(resources.getColor(R.color.red))
+                    val view = View.inflate(this, R.layout.dialog_bmi_solutions, null)
+                    val builder = AlertDialog.Builder(this)
+                    builder.setView(view)
+                    val dialog = builder.create()
+                    dialog.show()
+                    dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+                    view.cancel.setOnClickListener {
+                        dialog.dismiss()
                     }
-                    val genderBmiText=bottomSheetView.findViewById<TextView>(R.id.gender)
-                    if(man){
-                        genderBmiText.setTextColor(resources.getColor(R.color.blue))
-                        genderBmiText.text="Male"
+                    view.resultBmi.text=bmivalue.toString()
+                    view.status.text = statusBmi
+                    if (statusBmi == "Thin") {
+                        view.resultBmi.setTextColor(resources.getColor(R.color.light_blue))
+                    } else if (statusBmi == "Normal") {
+                        view.resultBmi.setTextColor(resources.getColor(R.color.green))
 
-                    }else{
-                        genderBmiText.setTextColor(resources.getColor(R.color.pink))
-                        genderBmiText.text="Female"
+                    } else {
+                        view.resultBmi.setTextColor(resources.getColor(R.color.red))
                     }
-                    bottomSheetView.findViewById<View>(R.id.linearLayout2).setOnClickListener {
-                        bottomSheet.dismiss()
-                    }
-                    bottomSheet.setContentView(bottomSheetView)
-                    bottomSheet.show()
+                    if (man) {
+                        view.genderText.text = "Male"
+                        view.genderImage.setImageResource(R.drawable.man)
+                        view.genderImage.setColorFilter(resources.getColor(R.color.light_blue))
 
+                    } else {
+                        view.genderText.text = "Female"
+                        view.genderImage.setColorFilter(resources.getColor(R.color.pink))
+                        view.genderImage.setImageResource(R.drawable.girl)
+                    }
                 }
-
-
-
             }
+
+
+
+
+
         }
-
-        backButton.setOnClickListener {
-            finish()
-        }
-
-
     }
 }
 
