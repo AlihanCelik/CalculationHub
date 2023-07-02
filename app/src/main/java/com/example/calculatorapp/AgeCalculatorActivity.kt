276e546fec8b729ab86504f2c265c9fcdfa9be17
Alihan <alihan_08032003@hotmail.com>
2023-07-03 01:26:03 +0300

    Calculator

--- app/src/main/java/com/example/calculatorapp/AgeCalculatorActivity.kt
+++ app/src/main/java/com/example/calculatorapp/AgeCalculatorActivity.kt
@@ -1,44 +1,52 @@
 package com.example.calculatorapp
 
+
 import android.app.AlertDialog
-import android.icu.text.SimpleDateFormat
-import android.icu.util.Calendar
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.NumberPicker
+import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import kotlinx.android.synthetic.main.activity_age_calculator.*
+import kotlinx.android.synthetic.main.bottom_sheet_agedate.view.*
 import kotlinx.android.synthetic.main.dialog_age_solutions.view.*
 import java.util.*
 
 class AgeCalculatorActivity : AppCompatActivity() {
-    var birth_month_value=0
-    var birth_day_value=0
-    var birth_year_value=0
+    var birth_month_value = 1
+    var birth_day_value = 1
+    var birth_year_value = 2000
 
-    var current_month_value=0
-    var current_day_value=0
-    var current_year_value=0
+    var current_month_value = 0
+    var current_day_value = 0
+    var current_year_value = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_age_calculator)
-        var c = Calendar.getInstance()
-        var cDay = c.get(Calendar.DAY_OF_MONTH)
-        var cMonth = c.get(Calendar.MONTH)
-        var cYear = c.get(Calendar.YEAR)
-        current_date.text="$cDay / ${getMonthName(cMonth)} / $cYear"
+
+        val c = Calendar.getInstance()
+        val cDay = c.get(Calendar.DAY_OF_MONTH)
+        val cMonth = c.get(Calendar.MONTH) + 1
+        val cYear = c.get(Calendar.YEAR)
+        current_date.text = "$cDay / ${getMonthName(cMonth)} / $cYear"
+        current_month_value = cMonth
+        current_day_value = cDay
+        current_year_value = cYear
+
         backButton.setOnClickListener {
             finish()
         }
+
         birth_dateSelect.setOnClickListener {
             val bottomSheet = BottomSheetDialog(this@AgeCalculatorActivity, R.style.BottomSheetDialogTheme)
             val bottomSheetView = LayoutInflater.from(applicationContext).inflate(
                 R.layout.bottom_sheet_agedate,
                 findViewById(R.id.bottomSheetAgeDate)
             ) as ConstraintLayout
+
             val month = bottomSheetView.findViewById<NumberPicker>(R.id.month)
             val year = bottomSheetView.findViewById<NumberPicker>(R.id.year)
             val date = bottomSheetView.findViewById<NumberPicker>(R.id.date)
@@ -49,6 +57,7 @@ class AgeCalculatorActivity : AppCompatActivity() {
             val currentYear = Calendar.getInstance().get(Calendar.YEAR)
             year.maxValue = currentYear
             year.minValue = 1900
+            year.value=2000
 
             year.setOnValueChangedListener { _, _, yearValue ->
                 val isLeapYear = yearValue % 4 == 0
@@ -77,16 +86,16 @@ class AgeCalculatorActivity : AppCompatActivity() {
                 else -> 31
             }
             date.maxValue = maxDay
-            date.minValue=1
+            date.minValue = 1
 
             bottomSheetView.findViewById<View>(R.id.linearLayout2).setOnClickListener {
                 bottomSheet.dismiss()
             }
             bottomSheetView.findViewById<View>(R.id.linearLayout3).setOnClickListener {
                 val selectedDate = "${date.value} / ${getMonthName(month.value)} / ${year.value}"
-                birth_month_value=month.value
-                birth_day_value=date.value
-                birth_year_value=year.value
+                birth_month_value = month.value
+                birth_day_value = date.value
+                birth_year_value = year.value
                 birth_date.text = selectedDate
                 bottomSheet.dismiss()
             }
@@ -96,22 +105,27 @@ class AgeCalculatorActivity : AppCompatActivity() {
             bottomSheet.setCanceledOnTouchOutside(true)
             bottomSheet.show()
         }
+
         current_dateSelect.setOnClickListener {
             val bottomSheet = BottomSheetDialog(this@AgeCalculatorActivity, R.style.BottomSheetDialogTheme)
             val bottomSheetView = LayoutInflater.from(applicationContext).inflate(
                 R.layout.bottom_sheet_agedate,
                 findViewById(R.id.bottomSheetAgeDate)
             ) as ConstraintLayout
+
             val month = bottomSheetView.findViewById<NumberPicker>(R.id.month)
             val year = bottomSheetView.findViewById<NumberPicker>(R.id.year)
             val date = bottomSheetView.findViewById<NumberPicker>(R.id.date)
 
             month.maxValue = 12
             month.minValue = 1
+            month.value= Calendar.getInstance().get(Calendar.MONTH)
 
 
+            val currentYear = Calendar.getInstance().get(Calendar.YEAR)
             year.maxValue = 2100
             year.minValue = 1900
+            year.value=currentYear
 
             year.setOnValueChangedListener { _, _, yearValue ->
                 val isLeapYear = yearValue % 4 == 0
@@ -132,7 +146,6 @@ class AgeCalculatorActivity : AppCompatActivity() {
                 date.maxValue = maxDay
             }
 
-
             val isLeapYear = year.value % 4 == 0
             val maxDay = when (month.value) {
                 1, 3, 5, 7, 8, 10, 12 -> 31
@@ -141,16 +154,17 @@ class AgeCalculatorActivity : AppCompatActivity() {
                 else -> 31
             }
             date.maxValue = maxDay
-            date.minValue=1
+            date.minValue = 1
+            date.value=Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
 
             bottomSheetView.findViewById<View>(R.id.linearLayout2).setOnClickListener {
                 bottomSheet.dismiss()
             }
             bottomSheetView.findViewById<View>(R.id.linearLayout3).setOnClickListener {
                 val selectedDate = "${date.value} / ${getMonthName(month.value)} / ${year.value}"
-                current_month_value=month.value
-                current_day_value=date.value
-                current_year_value=year.value
+                current_month_value = month.value
+                current_day_value = date.value
+                current_year_value = year.value
                 current_date.text = selectedDate
                 bottomSheet.dismiss()
             }
@@ -160,14 +174,16 @@ class AgeCalculatorActivity : AppCompatActivity() {
             bottomSheet.setCanceledOnTouchOutside(true)
             bottomSheet.show()
         }
+
         linearLayout2.setOnClickListener {
-            val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+            val dateFormat = java.text.SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
             val birthDateStr = "$birth_day_value/$birth_month_value/$birth_year_value"
             val birthDate = dateFormat.parse(birthDateStr)
 
             val currentDateStr = "$current_day_value/$current_month_value/$current_year_value"
             val currentDate = dateFormat.parse(currentDateStr)
             val age = currentDate.time - birthDate.time
+
             val ageInDays = age / (1000 * 60 * 60 * 24)
             val ageInMonths = ageInDays / 30
             val ageInYears = ageInDays / 365
@@ -180,10 +196,14 @@ class AgeCalculatorActivity : AppCompatActivity() {
             view.cancel.setOnClickListener {
                 dialog.dismiss()
             }
-            view.age_answer.text=ageInYears.toString()
-            view.ageMonth_Day.text="$ageInMonths Month / $ageInDays Day"
+            view.age_answer.text = ageInYears.toString()
+            view.year_answer.text=ageInYears.toString()
+            view.month_answer.text=ageInMonths.toString()
+            view.day_answer.text=ageInDays.toString()
+
         }
     }
+
     private fun getMonthName(month: Int): String {
         return when (month) {
             1 -> "January"
