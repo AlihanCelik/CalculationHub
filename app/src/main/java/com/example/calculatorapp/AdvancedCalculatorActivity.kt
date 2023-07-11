8c9bc55ffc7e2365584e9f5e604e9c2aa44b9ab8
Alihan <alihan_08032003@hotmail.com>
2023-07-12 00:28:19 +0300

    Calculator

--- app/src/main/java/com/example/calculatorapp/AdvancedCalculatorActivity.kt
+++ app/src/main/java/com/example/calculatorapp/AdvancedCalculatorActivity.kt
@@ -9,7 +9,6 @@ import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_advanced_calculator.*
 import java.math.BigDecimal
-import java.math.BigInteger
 import java.text.DecimalFormat
 import kotlin.math.*
 
